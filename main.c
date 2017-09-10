06db2ea91ae4fe9261f541c2c9b5d63b2867a8de
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-09-10 00:45:00 -0300

    Added insertion sort

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ void bubbleSort(int a[], int size);
 
 void processSelectedOption(int option);
 
+void insertionSort(int a[], int size);
+
 void printArray(int a[], int size);
 
 void restoreArrayValues();
@@ -16,10 +18,11 @@ int a[] = {2, 6, 7, 3, 4, 1};
 
 static const int OPTION_SELECTION_SORT = 1;
 static const int OPTION_BUBBLE_SORT = 2;
-static const int OPTION_EXIT = 3;
+static const int OPTION_INSERTION_SORT = 3;
+static const int OPTION_EXIT = 0;
 
 int main() {
-    int selectedOption = 0;
+    int selectedOption = -1;
     do {
         printf("\n\nWelcome to the super-duper-amazing sort program I've made because I couldn't remember almost any"
                        " kind of sorting algorithm :D\n\n");
@@ -27,13 +30,14 @@ int main() {
         printf("Options:\n");
         printf("1. Use selection sort\n");
         printf("2. Use bubble sort\n");
-        printf("3. Exit\n\n");
+        printf("3. Use insertion sort\n");
+        printf("0. Exit\n\n");
 
         scanf("\n%d", &selectedOption);
 
         processSelectedOption(selectedOption);
 
-    } while (selectedOption != 3);
+    } while (selectedOption != 0);
 
     return 0;
 }
@@ -51,6 +55,10 @@ void processSelectedOption(int option) {
 
         case OPTION_BUBBLE_SORT:
             bubbleSort(a, size);
+            break;
+
+        case OPTION_INSERTION_SORT:
+            insertionSort(a, size);
             break;
 
         case OPTION_EXIT:
@@ -107,5 +115,20 @@ void bubbleSort(int a[], int size) {
                 a[j + 1] = temp;
             }
         }
+    }
+}
+
+void insertionSort(int a[], int size) {
+    int value, holeIndex = 0;
+    for (int i = 1; i < size; i++) {
+        value = a[i];
+        holeIndex = i;
+
+        while (holeIndex > 0 && a[holeIndex - 1] > value) {
+            a[holeIndex] = a[holeIndex - 1];
+            holeIndex--;
+         }
+
+        a[holeIndex] = value;
     }
 }
